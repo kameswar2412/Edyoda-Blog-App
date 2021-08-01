8e9925372a30781ba10137c2f20be28d3b87ce92
Your Name <youremail@domain.com>
2021-08-01 16:47:14 +0530

    update1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css"
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Footer from './components/Footer/footer';
+import Header from './components/Header/header';
+import Home from './components/Home/home';
+import Course from './components/Course/course';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  return ( 
+    <div>
+      <BrowserRouter>
+      <Header />
+
+      <Switch>
+         <Route path="/" exact component={Home}/>
+         <Route path="/Course/:slug" component={Course} />
+         <Route component={()=> <h1>404 page not found</h1> } />
+
+      </Switch>
+      <Footer />
+      </BrowserRouter>
     </div>
-  );
+   );
 }
-
+ 
 export default App;
